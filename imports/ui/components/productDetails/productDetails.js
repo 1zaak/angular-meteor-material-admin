240cc8faa9e16ec1270d5e9cfb5e88504dcfcb2a
Izaak Khaliq <izaak@izaak.my>
2016-04-22 17:35:41 +0800

    Complete Chapter 6 Bind One Object

--- imports/ui/components/productDetails/productDetails.js
+++ imports/ui/components/productDetails/productDetails.js
@@ -1,36 +1,66 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
- 
+
 import './productDetails.html';
- 
+import { Products } from '../../../api/products';
+
+
 class ProductDetails {
-  constructor($stateParams) {
-    'ngInject';
-    
-    this.productId = $stateParams.productId;
-    this.name = $stateParams.productName;
-  }
+    constructor($stateParams, $scope, $reactive) {
+        'ngInject';
+
+        $reactive(this).attach($scope);
+
+
+        this.productId = $stateParams.productId;
+
+
+        this.helpers({
+            product() {
+                return Products.findOne({
+                    _id: $stateParams.productId
+                });
+            }
+        });
+    }
+    save() {
+        console.log("Save running");
+        Products.update({
+            _id: this.product._id
+        }, {
+            $set: {
+                name: this.product.name,
+                price: this.product.price
+            }
+        }, (error) =>{
+            if (error) {
+                console.log('Oops, unable to update the party...');
+            } else {
+                console.log('Done!');
+            }
+        })
+    }
 }
- 
+
 const name = 'productDetails';
- 
+
 // create a module
 export default angular.module(name, [
-  angularMeteor,
-  uiRouter
-]).component(name, {
-  templateUrl: `imports/ui/components/${name}/${name}.html`,
-  controllerAs: name,
-  controller: ProductDetails
-})
-  .config(config);
- 
+        angularMeteor,
+        uiRouter
+    ]).component(name, {
+        templateUrl: `imports/ui/components/${name}/${name}.html`,
+        controllerAs: name,
+        controller: ProductDetails
+    })
+    .config(config);
+
 function config($stateProvider) {
-  'ngInject';
- 
-  $stateProvider.state('productDetails', {
-    url: '/products/:productId/:productName',
-    template: '<product-details></product-details>'
-  });
+    'ngInject';
+
+    $stateProvider.state('productDetails', {
+        url: '/products/:productId',
+        template: '<product-details></product-details>'
+    });
 }
